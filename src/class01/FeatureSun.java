1ab2a068199b9c31d572671040e345684cae21b7
zulfiya-amrul <zulfiya.amrul@gmail.com>
2022-05-15 11:36:00 -0700

    the feature sun has been added successfully

---
+++ src/class01/FeatureSun.java
@@ -0,0 +1,9 @@
+package class01;
+
+public class FeatureSun {
+    String heat;
+    String radius;
+    String color;
+
+
+}
--- src/class01/Main.java
+++ src/class01/Main.java
@@ -7,5 +7,12 @@ public class Main {
         FactorX.moon = "4";
         FactorX.planets = "2";
         FactorX.stars = "10000";
+
+        // adding features to our sun
+        FeatureSun xFact= new FeatureSun();
+        xFact.color= "green";
+        xFact.radius= "10000";
+        xFact.heat= "1203oi4";
+        
     }
 }
